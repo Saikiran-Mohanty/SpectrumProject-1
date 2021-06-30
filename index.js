dbd5d88aa5491003c4cf05d0777dcee818529e63
Sai <saikiranmohanty@gmail.com>
2021-06-30 14:01:21 +0530

    Initial commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function eyeball(){
         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
         let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
         let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * -1) + 0;
+        let rot = (radian * (180 / Math.PI) * -1) + 270;
         eye.style.transform = "rotate("+ rot +"deg)"
 
     });
